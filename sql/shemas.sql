a54c41592fa051faa70a50da2d0ab12971bc6843
writedev <locxistwitch@gmail.com>
2025-06-17 18:43:30 +0200

    add sql and gitignore

---
+++ sql/shemas.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE HackMySignIn;
+
+USE HackMySignIn;
+
+CREATE TABLE users (
+    id CHAR(36) UNIQUE NOT NULL DEFAULT (UUID()),
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
